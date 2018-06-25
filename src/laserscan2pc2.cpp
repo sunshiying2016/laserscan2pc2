9a7d2f5eebbb4e890eee2c0de13d32333b99149e
sunshiying2016 <462233234@qq.com>
2018-06-25 23:50:27 +0800

    first commit

---
+++ src/laserscan2pc2.cpp
@@ -0,0 +1,63 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <laser_geometry/laser_geometry.h>
+#include <tf/transform_listener.h>
+#include <iostream>
+
+
+class lasersan_pc2_transformer
+{
+private:
+  ros::NodeHandle n_;
+  ros::Subscriber sub_;
+  ros::Publisher pub_;
+  laser_geometry::LaserProjection projector_;
+  tf::TransformListener listener_;
+  
+public:
+  lasersan_pc2_transformer()
+  {}
+  
+  ~lasersan_pc2_transformer()
+  {}
+  
+  void init()
+  {
+    sub_ = n_.subscribe("/spencer/sensors/laser_front/echo0", 1000, &lasersan_pc2_transformer::callback, this);
+    pub_ = n_.advertise<sensor_msgs::PointCloud2>("/obstacles", 1000);
+  }
+  
+  void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
+  {
+    if(!listener_.waitForTransform(
+        msg->header.frame_id,
+        "/odom",
+        msg->header.stamp + ros::Duration().fromSec(msg->ranges.size()*msg->time_increment),
+        ros::Duration(1.0)))
+    {
+     return;
+    }
+    
+    std::cout<<"!!!!!!!!!!!!!!!!!"<<std::endl;
+    
+    sensor_msgs::PointCloud2 cloud;
+    projector_.transformLaserScanToPointCloud("/odom",*msg,
+	    cloud,listener_);
+    
+    pub_.publish(cloud);
+  }
+  
+};
+
+
+
+int main(int argc, char ** argv)
+{
+  ros::init(argc, argv, "lasersan2pc2");
+  lasersan_pc2_transformer trans;
+  trans.init();
+  
+  ros::spin();
+  return 0;
+}
